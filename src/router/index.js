44a1f564686941c749a1d39f30b661bba057bea1
alicelukas <alicelukas@naver.com>
2018-12-25 22:53:51 +0900

    첫 업로드

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  hash: false,
+  mode: 'history',
+  linkActiveClass: "active",
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
